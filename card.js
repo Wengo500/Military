82c35cb55c5fb71dd99f1486b3cfc553b429bc63
Wengo500 <wengo500@gmail.com>
2020-09-17 15:17:08 +0300

    commit

--- card.js
+++ card.js
@@ -43,11 +43,27 @@ let createCard = (img, type) => {
     squadStamina.appendChild(stateStamina);
 
 };
-createCard(knightBg, 'knight');
 createCard(archerBg, 'archer');
+createCard(knightBg, 'knight');
 createCard(cavalryBg, 'cavalry');
 
+document.getElementsByClassName('createUnit')[0].addEventListener('click', () => {
+    archer.push(archer[0].clone());
+});
+document.getElementsByClassName('createUnit')[1].addEventListener('click', () => {
+    knight.push(knight[0].clone());
+});
+document.getElementsByClassName('createUnit')[2].addEventListener('click', () => {
+    cavalry.push(cavalry[0].clone());
+});
 
+const mathHealth = (array, res = 0) => {
+    array.forEach(element => {
+        res += element.health;
+        console.log(res);
+    });
+}
+mathHealth(archer);
 
 const stateHealthFnc = (a = 100, b = 100, c = 100) => {
 
@@ -60,7 +76,6 @@ const stateHealthFnc = (a = 100, b = 100, c = 100) => {
     document.getElementsByClassName('squad_health')[2].addEventListener('click', () => {
         document.getElementsByClassName("state_health")[2].style.width = `${c -= 10}%`;
     });
-
 };
 stateHealthFnc();
 
@@ -77,21 +92,3 @@ const stateStaminaFnc = (a = 100, b = 100, c = 100) => {
     });
 };
 stateStaminaFnc();
-
-document.getElementsByClassName('createUnit')[0].addEventListener('click', () => {
-    for (let i = 0; i < 1; i++) {
-        archer.push(archer[0].clone());
-    }
-});
-document.getElementsByClassName('createUnit')[1].addEventListener('click', () => {
-    for (let i = 0; i < 1; i++) {
-        knight.push(knight[0].clone());
-
-    }
-});
-document.getElementsByClassName('createUnit')[2].addEventListener('click', () => {
-    for (let i = 0; i < 1; i++) {
-        cavalry.push(cavalry[0].clone());
-
-    }
-});
